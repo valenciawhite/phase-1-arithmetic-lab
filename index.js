7d44af9e530995057dd5beb765053bbb4fc61bb1
Valencia White <valenciawhite@rocketmail.com>
2022-02-02 00:08:49 -0500

    Comleted assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 22;
+let num4 = 6;
+let mod = num3 % num4; 
+
+let max = Math.max(5, 10, 15, 20);
